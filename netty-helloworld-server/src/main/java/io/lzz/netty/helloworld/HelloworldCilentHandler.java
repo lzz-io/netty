058ce06e26aa16ebbd94bf563b30c16a437fd5a1
lzz.io <1219331697@qq.com>
2018-09-30 03:42:27 +0800

    hello world server

---
+++ netty-helloworld-server/src/main/java/io/lzz/netty/helloworld/HelloworldCilentHandler.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright qq:1219331697
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.lzz.netty.helloworld;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author q1219331697
+ *
+ */
+public class HelloworldCilentHandler extends SimpleChannelInboundHandler<Object> {
+
+	private ByteBuf content;
+	private ChannelHandlerContext ctx;
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		this.ctx = ctx;
+
+		// Initialize the message.
+		content = ctx.alloc().directBuffer(HelloworldCilent.SIZE).writeZero(HelloworldCilent.SIZE);
+
+		// Send the initial messages.
+		generateTraffic();
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) {
+		content.release();
+	}
+
+	@Override
+	public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// Server is supposed to send nothing, but if it sends something, discard it.
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	long counter;
+
+	private void generateTraffic() {
+		// Flush the outbound buffer to the socket.
+		// Once flushed, generate the same amount of traffic again.
+		ctx.writeAndFlush(content.retainedDuplicate()).addListener(trafficGenerator);
+	}
+
+	private final ChannelFutureListener trafficGenerator = new ChannelFutureListener() {
+		@Override
+		public void operationComplete(ChannelFuture future) {
+			if (future.isSuccess()) {
+				generateTraffic();
+			} else {
+				future.cause().printStackTrace();
+				future.channel().close();
+			}
+		}
+	};
+
+}
