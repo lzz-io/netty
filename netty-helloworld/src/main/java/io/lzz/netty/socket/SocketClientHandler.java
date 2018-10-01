d312c674b9d85ab7ea8398a8dba051977e0c3017
lzz.io <1219331697@qq.com>
2018-10-01 20:13:55 +0800

    socket example

---
+++ netty-helloworld/src/main/java/io/lzz/netty/socket/SocketClientHandler.java
@@ -0,0 +1,27 @@
+package io.lzz.netty.socket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class SocketClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+        ctx.channel().writeAndFlush("from client :" + LocalDateTime.now());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.channel().writeAndFlush("hello");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
